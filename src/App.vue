41c341976b304acdd8082aee9b881c16aa969603
Hosni_Cr <105177254+HosniDEV@users.noreply.github.com>
2024-01-16 14:40:00 +0100

    adding scroll button

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,38 @@ import ServicesItem from "./components/ServicesItem.vue";
 import PortifolioItem from "./components/PortifolioItem.vue";
 import AboutItem from "./components/AboutItem.vue";
 import ContactItem from "./components/ContactItem.vue";
+import { ChevronUpIcon } from "@heroicons/vue/24/outline";
+import { ref, onMounted, onUnmounted } from "vue";
+
+const showScrollButton = ref(true);
+const handleScroll = () => {
+  showScrollButton.value = window.scrollY > 200; // Adjust the scroll position threshold as needed
+};
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+// Remove the scroll event listener when the component is destroyed
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
 </script>
 
 <template>
-  <div class="bg-gray-100">
+  <div class="" id="main">
+    <button
+      v-show="showScrollButton"
+      @click="scrollToTop"
+      class="fixed bg-black/90 rounded-full p-2 text-white bottom-4 right-4"
+    >
+      <ChevronUpIcon class="size-6 animate-bounce" />
+    </button>
     <NabarItem />
     <LandingItem />
     <main>
